20fd438f21b76233b560c95ef46cc7692e3cd3b9
Fizjopatka <dragonnaster@gmail.com>
2020-10-18 15:09:29 +0200

    Wersja po 2 poprawkach

--- app.js
+++ app.js
@@ -2,10 +2,8 @@
 
 areYouView();
 
-function introAnimations(){
-
+function introAnimations() {
     const tl = gsap.timeline({defaults: {ease: 'power1.out'} } );
-
     tl.to('.text', {y: '0%', duration: 1, stagger: 1});
     tl.from('#passion', {duration: 0.6, color:'white'});
     tl.to('.intro-slider', {x:'-100%', duration: 1, delay: 0.5});
@@ -18,30 +16,25 @@ function introAnimations(){
     tl.fromTo('.hours-container-xs', {opacity: 0}, {opacity: 1, duration: 0.5}, 'hours');
     tl.fromTo('.slide-col', {opacity: 0}, {opacity: 1, duration: 0.3, stagger: 0.5}, 'sliders');
     tl.fromTo('.uslugi-xs', {opacity: 0}, {opacity: 1, duration: 0.3, stagger: 0.5},'sliders');
-
     localStorage.setItem('view', 'yes');
 }
 
 // HAVE YOU WATCHED INTRO IN LAST 12H?
 
 function areYouView() {
-
-    let isView = localStorage.getItem('view');
-
-    if (isView === null) {
+    const IS_VIEW = localStorage.getItem('view');
+    if (IS_VIEW === null) {
         $('.intro').css('display', 'flex');
         introAnimations();
-        const timeEntrance = Date.now();
-        localStorage.setItem('TimeEntrance', timeEntrance);
+        const TIME_ENTRANCE = Date.now();
+        localStorage.setItem('TimeEntrance', TIME_ENTRANCE);
     } else {
         $('.intro').css('display', 'none');
-
-        const intTimeEntrance = parseInt(localStorage.getItem('TimeEntrance'));
+        const TIME_NOW = Date.now();
         const TWELVE_HOURS_IN_MILLISEC = 43200000;
+        const intTimeEntrance = parseInt(localStorage.getItem('TimeEntrance'));
         const timeAfter12h = intTimeEntrance + TWELVE_HOURS_IN_MILLISEC;
-        const timeNow = Date.now();
-        
-        if(timeNow > timeAfter12h) {
+        if(TIME_NOW > timeAfter12h) {
             localStorage.removeItem('view');
             areYouView();
         };
@@ -50,72 +43,67 @@ function areYouView() {
 
 //WHAT TODAY IS DAY 
 
-const today = new Date();
-const dayOfWeek = today.getDay();
-const hourNow = today.getHours();
-
-$('.d'+dayOfWeek).css('background-color', 'rgba(206, 29, 29, 0.6)');
+const TODAY= new Date();
+const DAY_OF_WEEK = TODAY.getDay();
+const HOUR_NOW = TODAY.getHours();
+$('.d'+ DAY_OF_WEEK).css('background-color', 'rgba(206, 29, 29, 0.6)');
 
 //HOURS-CONTAINER TITLE ATTRIBUTE
+
 const STILL_CLOSE_INFO = 'Jeszcze zamknięte. Zapraszamy dziś od 9.';
 const ALREADY_CLOSE_INFO = 'Już zamknięte. Zapraszamy jutro od 9.';
 const OPEN_INFO = 'Otwarte. Zapraszamy do warsztatu, lub do kontaktu telefonicznego.';
 const ALREADY_CLOSE_SATURDAY_INFO = 'Już zamknięte. Zapraszamy w poniedziałek od 9.';
 const SUNDAY_INFO = 'Zamknięte. Zapraszamy jutro od 9.';
 
-function normalDay(hourNow){
-    if (hourNow < 9){
+function normalDay(HOUR_NOW) {
+    if (HOUR_NOW < 9) {
         $('.hours-container-xs').attr('title', STILL_CLOSE_INFO);
         $('.hours-container').attr('title', STILL_CLOSE_INFO);
 
-    }
-    else if (hourNow > 16 ){
+    } else if (HOUR_NOW > 16 ) {
         $('.hours-container-xs').attr('title', ALREADY_CLOSE_INFO );
         $('.hours-container').attr('title', ALREADY_CLOSE_INFO );
 
-    }
-    else{
+    } else {
         $('.hours-container').attr('title', OPEN_INFO);
         $('.hours-container-xs').attr('title', OPEN_INFO);
     };
-
 };
-function saturDay(hourNow){
-    if (hourNow < 9){
+
+function saturDay(HOUR_NOW) {
+    if (HOUR_NOW < 9) {
         $('.hours-container-xs').attr('title', STILL_CLOSE_INFO);
         $('.hours-container').attr('title', STILL_CLOSE_INFO);
 
-    }
-    else if (hourNow > 13 ){
+    } else if (HOUR_NOW > 13 ) {
         $('.hours-container-xs').attr('title', ALREADY_CLOSE_SATURDAY_INFO);
         $('.hours-container').attr('title', ALREADY_CLOSE_SATURDAY_INFO);
 
-    }
-    else{
+    } else {
         $('.hours-container').attr('title', OPEN_INFO);
         $('.hours-container-xs').attr('title', OPEN_INFO);
     };
-
 };
 
-switch (dayOfWeek){
+switch (DAY_OF_WEEK) {
     case 1:
-        normalDay(hourNow);
+        normalDay(HOUR_NOW);
         break;
     case 2:
-        normalDay(hourNow);  
+        normalDay(HOUR_NOW);  
         break;      
     case 3:
-        normalDay(hourNow);
+        normalDay(HOUR_NOW);
         break;
     case 4:
-        normalDay(hourNow);
+        normalDay(HOUR_NOW);
         break;
     case 5:
-        normalDay(hourNow);
+        normalDay(HOUR_NOW);
         break;
     case 6:
-        saturDay(hourNow);
+        saturDay(HOUR_NOW);
         break;
     case 0:
         $('.hours-container-xs').attr('title', SUNDAY_INFO);
@@ -124,22 +112,19 @@ switch (dayOfWeek){
 }
 
 //OPTIONS LISTENERS
-const logo = document.querySelector('#logo')
 
+const logo = document.querySelector('#logo')
 const op1 = document.querySelector('#option-1');
 const op2 = document.querySelector('#option-2');
 const op3 = document.querySelector('#option-3');
-
 const op1xs = document.querySelector('#optionxs-1');
 const op2xs = document.querySelector('#optionxs-2');
 const op3xs = document.querySelector('#optionxs-3');
 
 logo.addEventListener('click', ()=> window.location.assign('index.html'));
-
 op1.addEventListener('click', ()=> window.location.assign('oferta.html'));
 op2.addEventListener('click', ()=> window.location.assign('lokalizacja.html'));
 op3.addEventListener('click', ()=> window.location.assign('kontakt.html'));
-
 op1xs.addEventListener('click', ()=> window.location.assign('oferta.html'));
 op2xs.addEventListener('click', ()=> window.location.assign('lokalizacja.html'));
 op3xs.addEventListener('click', ()=> window.location.assign('kontakt.html'));
