4a8e86f47f81eaf4e265c7b8641e2cb75d459c8b
Fizjopatka <dragonnaster@gmail.com>
2020-10-31 20:03:33 +0100

    new const, setInfoAboutDay, fonts, resp under 390px

--- html/script/app.js
+++ html/script/app.js
@@ -1,122 +1,14 @@
-// INTRO ANIMATION
-
-areYouView();
-
-function introAnimations() {
-    const tl = gsap.timeline({defaults: {ease: 'power1.out'} } );
-    tl.to('.text', {y: '0%', duration: 1, stagger: 1});
-    tl.from('#passion', {duration: 0.6, color:'white'});
-    tl.to('.intro-slider', {x:'-100%', duration: 1, delay: 0.5});
-    tl.to('.intro', {x: '-100%', duration: 1, delay: -1});
-    tl.fromTo('#logo-container', {opacity: 0}, {opacity: 1, duration: 1}, 'anim1');
-    tl.fromTo('section', {opacity: 0.2}, {opacity: 1, duration: 1}, 'anim1');
-    tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 0.8, delay: 0.5}, 'anim1');
-    tl.fromTo('footer', {opacity: 0}, {opacity: 1, duration: 1}, 'anim1');
-    tl.fromTo('.hours-container', {opacity: 0}, {opacity: 1, duration: 0.5}, 'hours');
-    tl.fromTo('.hours-container-xs', {opacity: 0}, {opacity: 1, duration: 0.5}, 'hours');
-    tl.fromTo('.slide-col', {opacity: 0}, {opacity: 1, duration: 0.3, stagger: 0.5}, 'sliders');
-    tl.fromTo('.uslugi-xs', {opacity: 0}, {opacity: 1, duration: 0.3, stagger: 0.5},'sliders');
-    localStorage.setItem('view', 'yes');
-}
-
-// HAVE YOU WATCHED INTRO IN LAST 12H?
-
-function areYouView() {
-    const IS_VIEW = localStorage.getItem('view');
-    
-    if (IS_VIEW === null) {
-        const TIME_ENTRANCE = Date.now();
-
-        $('.intro').css('display', 'flex');
-        introAnimations();
-        localStorage.setItem('TimeEntrance', TIME_ENTRANCE);
-    } else {
-        const TIME_NOW = Date.now();
-        const TWELVE_HOURS_IN_MILLISEC = 43200000;
-        const intTimeEntrance = parseInt(localStorage.getItem('TimeEntrance'));
-        const timeAfter12h = intTimeEntrance + TWELVE_HOURS_IN_MILLISEC;
-
-        $('.intro').css('display', 'none');
-        if(TIME_NOW > timeAfter12h) {
-            localStorage.removeItem('view');
-            areYouView();
-        };
-    };
-};
-
-//WHAT TODAY IS DAY 
-
-const TODAY= new Date();
-const DAY_OF_WEEK = TODAY.getDay();
-const HOUR_NOW = TODAY.getHours();
-
-$('.d'+ DAY_OF_WEEK).css('background-color', 'rgba(206, 29, 29, 0.6)');
-
-//HOURS-CONTAINER TITLE ATTRIBUTE
-
-const STILL_CLOSE_INFO = 'Jeszcze zamknięte. Zapraszamy dziś od 9.';
-const ALREADY_CLOSE_INFO = 'Już zamknięte. Zapraszamy jutro od 9.';
-const OPEN_INFO = 'Otwarte. Zapraszamy do warsztatu lub kontaktu telefonicznego.';
-const ALREADY_CLOSE_SATURDAY_INFO = 'Już zamknięte. Zapraszamy w poniedziałek od 9.';
-const SUNDAY_INFO = 'Zamknięte. Zapraszamy jutro od 9.';
-
-function normalDay(HOUR_NOW) {
-    if (HOUR_NOW < 9) {
-        $('.hours-container-xs').attr('title', STILL_CLOSE_INFO);
-        $('.hours-container').attr('title', STILL_CLOSE_INFO);
-
-    } else if (HOUR_NOW > 16) {
-        $('.hours-container-xs').attr('title', ALREADY_CLOSE_INFO );
-        $('.hours-container').attr('title', ALREADY_CLOSE_INFO );
-
-    } else {
-        $('.hours-container').attr('title', OPEN_INFO);
-        $('.hours-container-xs').attr('title', OPEN_INFO);
-    };
-};
-
-function saturDay(HOUR_NOW) {
-    if (HOUR_NOW < 9) {
-        $('.hours-container-xs').attr('title', STILL_CLOSE_INFO);
-        $('.hours-container').attr('title', STILL_CLOSE_INFO);
-
-    } else if (HOUR_NOW > 13) {
-        $('.hours-container-xs').attr('title', ALREADY_CLOSE_SATURDAY_INFO);
-        $('.hours-container').attr('title', ALREADY_CLOSE_SATURDAY_INFO);
-
-    } else {
-        $('.hours-container').attr('title', OPEN_INFO);
-        $('.hours-container-xs').attr('title', OPEN_INFO);
-    };
-};
-
-switch (DAY_OF_WEEK) {
-    case 1:
-        normalDay(HOUR_NOW);
-        break;
-    case 2:
-        normalDay(HOUR_NOW);  
-        break;      
-    case 3:
-        normalDay(HOUR_NOW);
-        break;
-    case 4:
-        normalDay(HOUR_NOW);
-        break;
-    case 5:
-        normalDay(HOUR_NOW);
-        break;
-    case 6:
-        saturDay(HOUR_NOW);
-        break;
-    case 0:
-        $('.hours-container-xs').attr('title', SUNDAY_INFO);
-        $('.hours-container').attr('title', SUNDAY_INFO);
-        break;
-}
-
-//OPTIONS LISTENERS
-
+//CONST
+const today= new Date();
+const dayOfWeek = today.getDay();
+const hourNow = today.getHours();
+const timeEntrance = Date.now();
+const ANIM1 = 'anim1';
+const HOURS = 'hours';
+const SLIDERS = 'sliders';
+const HOURS_CONTAINER = '.hours-container';
+const HOURS_CONTAINER_XS = '.hours-container-xs';
+const TITLE = 'title';
 const logo = document.querySelector('#logo')
 const op1 = document.querySelector('#option-1');
 const op2 = document.querySelector('#option-2');
@@ -124,14 +16,100 @@ const op3 = document.querySelector('#option-3');
 const op1xs = document.querySelector('#optionxs-1');
 const op2xs = document.querySelector('#optionxs-2');
 const op3xs = document.querySelector('#optionxs-3');
+const CLICK = 'click';
+const INDEX = 'index.html';
+const OFFER = 'oferta.html';
+const LOCATION = 'lokalizacja.html';
+const CONTACT = 'kontakt.html';
 
-logo.addEventListener('click', () => window.location.assign('index.html'));
-op1.addEventListener('click', () => window.location.assign('oferta.html'));
-op2.addEventListener('click', () => window.location.assign('lokalizacja.html'));
-op3.addEventListener('click', () => window.location.assign('kontakt.html'));
-op1xs.addEventListener('click', () => window.location.assign('oferta.html'));
-op2xs.addEventListener('click', () => window.location.assign('lokalizacja.html'));
-op3xs.addEventListener('click', () => window.location.assign('kontakt.html'));
+//WHAT TODAY IS DAY
+$('.d'+ dayOfWeek).css('background-color', 'rgba(206, 29, 29, 0.6)');
+
+//FUNCTIONS
+function introAnimations() {
+    const tl = gsap.timeline({defaults: {ease: 'power1.out'} } );
+    tl.to('.text', {y: '0%', duration: 1, stagger: 1});
+    tl.from('#passion', {duration: 0.6, color:'white'});
+    tl.to('.intro-slider', {x:'-100%', duration: 1, delay: 0.5});
+    tl.to('.intro', {x: '-100%', duration: 1, delay: -1});
+    tl.fromTo('#logo-container', {opacity: 0}, {opacity: 1, duration: 1}, ANIM1);
+    tl.fromTo('section', {opacity: 0.2}, {opacity: 1, duration: 1}, ANIM1);
+    tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 0.8, delay: 0.5}, ANIM1);
+    tl.fromTo('footer', {opacity: 0}, {opacity: 1, duration: 1}, ANIM1);
+    tl.fromTo(HOURS_CONTAINER, {opacity: 0}, {opacity: 1, duration: 0.5}, HOURS);
+    tl.fromTo(HOURS_CONTAINER_XS, {opacity: 0}, {opacity: 1, duration: 0.5}, HOURS);
+    tl.fromTo('.slide-col', {opacity: 0}, {opacity: 1, duration: 0.3, stagger: 0.5}, SLIDERS);
+    tl.fromTo('.uslugi-xs', {opacity: 0}, {opacity: 1, duration: 0.3, stagger: 0.5}, SLIDERS);
+    localStorage.setItem('view', 'yes');
+};
+
+function areYouView() {
+    const isView = localStorage.getItem('view');
+    
+    if (isView === null) {
+        $('.intro').css('display', 'flex');
+        introAnimations();
+        localStorage.setItem('TimeEntrance', timeEntrance);
+    } else {
+        const TWELVE_HOURS_IN_MILLISEC = 43200000;
+        const intTimeEntrance = parseInt(localStorage.getItem('TimeEntrance'));
+        const timeAfter12h = intTimeEntrance + TWELVE_HOURS_IN_MILLISEC;
+
+        $('.intro').css('display', 'none');
+        if(timeEntrance > timeAfter12h) {
+            localStorage.removeItem('view');
+            areYouView();
+        };
+    };
+};
+
+function setInfoAboutDay(dayOfWeek, hourNow){
+    const STILL_CLOSE_INFO = 'Jeszcze zamknięte. Zapraszamy dziś od 9.';
+    const ALREADY_CLOSE_INFO = 'Już zamknięte. Zapraszamy jutro od 9.';
+    const OPEN_INFO = 'Otwarte. Zapraszamy do warsztatu lub kontaktu telefonicznego.';
+    const ALREADY_CLOSE_SATURDAY_INFO = 'Już zamknięte. Zapraszamy w poniedziałek od 9.';
+    const SUNDAY_INFO = 'Zamknięte. Zapraszamy jutro od 9.';
+    console.log(dayOfWeek)
+    console.log(hourNow)
+    if (dayOfWeek > 0 || dayOfWeek < 6){
+        if (hourNow < 9) {
+            $(HOURS_CONTAINER_XS).attr(TITLE, STILL_CLOSE_INFO);
+            $(HOURS_CONTAINER).attr(TITLE, STILL_CLOSE_INFO);
+        } else if (hourNow > 16) {
+            $(HOURS_CONTAINER_XS).attr(TITLE, ALREADY_CLOSE_INFO );
+            $(HOURS_CONTAINER).attr(TITLE, ALREADY_CLOSE_INFO );
+        } else {
+            $(HOURS_CONTAINER).attr(TITLE, OPEN_INFO);
+            $(HOURS_CONTAINER_XS).attr(TITLE, OPEN_INFO);
+        };
+    } else if (dayOfWeek === 6){
+        if (hourNow < 9) {
+            $(HOURS_CONTAINER_XS).attr(TITLE, STILL_CLOSE_INFO);
+            $(HOURS_CONTAINER).attr(TITLE, STILL_CLOSE_INFO);
+        } else if (hourNow > 13) {
+            $(HOURS_CONTAINER_XS).attr(TITLE, ALREADY_CLOSE_SATURDAY_INFO);
+            $(HOURS_CONTAINER).attr(TITLE, ALREADY_CLOSE_SATURDAY_INFO);
+        } else {
+            $(HOURS_CONTAINER).attr(TITLE, OPEN_INFO);
+            $(HOURS_CONTAINER_XS).attr(TITLE, OPEN_INFO);
+        };
+    } else {
+        $(HOURS_CONTAINER_XS).attr(TITLE, SUNDAY_INFO);
+        $(HOURS_CONTAINER).attr(TITLE, SUNDAY_INFO);
+    };
+};
+
+areYouView();
+setInfoAboutDay(dayOfWeek, hourNow);
+
+//OPTIONS LISTENERS
+logo.addEventListener(CLICK, () => window.location.assign(INDEX));
+op1.addEventListener(CLICK, () => window.location.assign(OFFER));
+op2.addEventListener(CLICK, () => window.location.assign(LOCATION));
+op3.addEventListener(CLICK, () => window.location.assign(CONTACT));
+op1xs.addEventListener(CLICK, () => window.location.assign(OFFER));
+op2xs.addEventListener(CLICK, () => window.location.assign(LOCATION));
+op3xs.addEventListener(CLICK, () => window.location.assign(CONTACT));
 
 //COOKIES
 // Skrypt wygenerowano za darmo z uzyciem: https://skrypt-cookies.pl  
